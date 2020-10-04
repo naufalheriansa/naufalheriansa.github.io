5e21f6d663cc2f989b54caf2101fdc3b6654b85f
naufalheriansa <naufalheriansa123@gmail.com>
2020-10-05 00:37:40 +0700

    Update sendEmail.php

--- sendEmail.php
+++ sendEmail.php
@@ -1,49 +1,28 @@
 <?php 
-use PHPMailer;
-
-if(isset($_POST['name']) && isset($_POST['email'])){
     $name = $_POST['name'];
     $email = $_POST['email'];
     $phone = $_POST['phone'];
     $subject = $_POST['subject'];
     $iq = $_POST['iq'];
 
-    require_once "PHPMailer/PHPMailer.php";
-    require_once "PHPMailer/SMTP.php";
-    require_once "PHPMailer/Exception.php";
-    require_once "PHPMailer/OAuth.php";
-    require_once "PHPMailer/POP3.php";
+    $email_from = 'wayneheriansa@gmail.com';
 
-    $mail = new PHPMailer();
+    $email_subject = "Hello Naufal";
 
-    //smtp settings
-    $mail->isSMTP();
-    $mail->Host = "smtp.gmail.com";
-    $mail->SMTPAuth = true;
-    $mail->Username = "naufal.14116110@student.itera.ac.id";
-    $mail->Password = 'naufal.14116110';
-    $mail->Port = 465;
-    $mail->SMTPSecure = "ssl";
+    $email_body =   "Name : $name. \n".
+                    "Email : $email. \n".
+                    "Phone : $phone. \n".
+                    "Subject : $subject. \n".
+                    "Message : $iq. \n";
 
-    //email settings
-    $mail->isHTML(true);
-    $mail->setFrom($email, $name);
-    $mail->addAddress("naufal.14116110@student.itera.ac.id");
-    $mail->Subject = ("$email ($subject)");
-    $mail->Body = $body;
+    $to = "naufalheriansa13@gmail.com";
 
-    if($mail->send()){
-        $status = "success";
-        $response = "Email is sent!";
-    }
-    else
-    {
-        $status = "failed";
-        $response = "Something is wrong: <br>" . $mail->ErrorInfo;
-    }
+    $headers = "From : $email_from \r \n";
 
-    exit(json_encode(array("status" => $status, "response" => $response)));
-}
+    $headers = "Reply-To : $email \r \n";
 
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("Location : index.html");
+    
 ?>
-   
